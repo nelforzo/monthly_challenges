1aef122bbfa9347fb87ec33910cf34251391e4c1
Frank Lopez <nelforzo@outlook.com>
2022-10-30 17:19:45 +0900

    sending back hardcoded html

--- challenges/views.py
+++ challenges/views.py
@@ -2,6 +2,7 @@
 from ctypes import sizeof
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 monthly_challenges = {
     'january': 'eat no meat for the entire month',
@@ -27,7 +28,8 @@ def monthly_challenge_by_number(request, month):
     if month > 0 and month <= len(monthly_challenges):
         months = list(monthly_challenges.keys())
         redirect_month = months[month - 1]
-        return HttpResponseRedirect('/challenges/' + redirect_month)
+        redirect_path = reverse('month-challenge', args=[redirect_month])
+        return HttpResponseRedirect(redirect_path)
     else:
         return HttpResponseNotFound(response_not_found_message)
 
@@ -36,6 +38,8 @@ def monthly_challenge(request, month):
     challenge_text = None
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_data = f'<h1>{challenge_text}</h1>'
+        return HttpResponse(response_data)
     except:
-        return HttpResponseNotFound(response_not_found_message)
+        response_data = f'<h1>{response_not_found_message}</h1>'
+        return HttpResponseNotFound(response_data)
